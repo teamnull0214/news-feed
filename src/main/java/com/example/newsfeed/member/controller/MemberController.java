a201ac1b58da00995dbf878d0e22ed1421ad9cb5
queenriwon <queenriwon3@gmail.com>
2025-02-15 01:50:33 +0900

    Feat(member): deleteMember() Service 작성
    입력한 비밀번호와 유저비밀번호를 확인하고 삭제하는 코드를 구현하였습니다.
    updateIsDelete()를 사용하여 soft delete를 구현했습니다.

--- src/main/java/com/example/newsfeed/member/controller/MemberController.java
+++ src/main/java/com/example/newsfeed/member/controller/MemberController.java
@@ -1,13 +1,56 @@
 package com.example.newsfeed.member.controller;
 
+import com.example.newsfeed.global.entity.SessionMemberDto;
+import com.example.newsfeed.member.dto.MemberRequestDto;
+import com.example.newsfeed.member.dto.MemberResponseDto;
+import com.example.newsfeed.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    /*회원가입 기능 구현*/
+    @PostMapping("/sign-up")
+    public ResponseEntity<MemberResponseDto> createMember(
+            @Valid @RequestBody MemberRequestDto dto
+    ) {
+        log.info("회원가입 API 호출");
+        return new ResponseEntity<>(memberService.createMember(
+                dto.getName(),
+                dto.getNickname(),
+                dto.getEmail(),
+                dto.getPassword(),
+                dto.getPasswordCheck()
+        ), HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/delete")
+    public ResponseEntity<Void> deleteMember(
+            @RequestBody Map<String, String> dto,
+            HttpServletRequest httpServletRequest
+    ) {
+        HttpSession session = httpServletRequest.getSession(false);
+        SessionMemberDto sessionMemberDto = (SessionMemberDto) session.getAttribute("member");
+        memberService.deleteMember(sessionMemberDto, dto.get("password"));
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }

4f89100d4a8a85815c95dc5a2ed455b03e825d61
abc <clvlajrrhtlvek@gmail.com>
2025-02-15 00:37:24 +0900

    Feat(member): 회원가입 기능 추가
    회원이 이미 존재하거나, 비밀번호와 비밀번호 확인이 일치하지 않을 경우 RuntimeException이 발생하도록 구현했습니다. 또한, 추후 추가할 비밀번호 암호화 부분을 주석으로 표시해 두었습니다.

--- src/main/java/com/example/newsfeed/member/controller/MemberController.java
+++ src/main/java/com/example/newsfeed/member/controller/MemberController.java
@@ -1,7 +1,15 @@
 package com.example.newsfeed.member.controller;
 
+import com.example.newsfeed.member.dto.MemberRequestDto;
+import com.example.newsfeed.member.dto.MemberResponseDto;
+import com.example.newsfeed.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    /*회원가입 기능 구현*/
+    @PostMapping("/sign-up")
+    public ResponseEntity<MemberResponseDto> createMember(
+            @Valid @RequestBody MemberRequestDto dto
+    ) {
+        log.info("회원가입 API 호출");
+        return new ResponseEntity<>(memberService.createMember(
+                dto.getName(),
+                dto.getNickname(),
+                dto.getEmail(),
+                dto.getPassword(),
+                dto.getPasswordCheck()
+        ), HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/newsfeed/member/service/MemberService.java
+++ src/main/java/com/example/newsfeed/member/service/MemberService.java
@@ -1,11 +1,55 @@
 package com.example.newsfeed.member.service;
 
+import com.example.newsfeed.member.dto.MemberResponseDto;
+import com.example.newsfeed.member.entity.Member;
+import com.example.newsfeed.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public MemberResponseDto createMember(
+            String name,
+            String nickName,
+            String email,
+            String password,
+            String passwordCheck
+    ) {
+        /*password와 passwordCheck가 일치하지 않는 경우*/
+        if (!password.equals(passwordCheck) ) {
+            throw new RuntimeException("비밀번호가 서로 일치하지 않습니다.");
+        }
+
+        /*이미 존재하는 회원인 경우*/
+        Optional<Member> memberByEmail = memberRepository.findMemberByEmail(email);
+        if (memberByEmail.isPresent()) {
+            throw new RuntimeException("이미 존재하는 회원입니다.");
+        }
+
+        /*member 객체 생성*/
+        Member member = new Member(name, nickName, email, password);
+        Member savedMember = memberRepository.save(member);
+
+        /*todo: 비밀번호 암호화 추가하기*/
+
+        log.info("유저 회원가입 성공");
+
+        return new MemberResponseDto(
+                savedMember.getId(),
+                savedMember.getUsername(),
+                savedMember.getNickname(),
+                savedMember.getEmail(),
+                savedMember.getCreatedAt()
+        );
+    }
 }

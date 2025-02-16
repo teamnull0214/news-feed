bafcbb6b983d65752f085cddfa10249fba4abf28
yjn33 <yjn0032@gmail.com>
2025-02-17 00:35:23 +0900

    feat(post-read): 전체게시글조회 api기능 구현 PostController.java -> findAllPosts 메서드 생성
    PostResponseDto.java -> 기존 클래스 생성자에 content 추가 및 JpaRepository에 전달할 toDto 메서드 생성
    
    PostService.java -> findAll 메서드 생성
    
    BaseDateTime.java -> 클래스 멤버변수명 수정 updateAt -> modifiedAt

--- src/main/java/com/example/newsfeed/post/controller/PostController.java
+++ src/main/java/com/example/newsfeed/post/controller/PostController.java
@@ -1,13 +1,36 @@
 package com.example.newsfeed.post.controller;
 
+import com.example.newsfeed.post.dto.PostResponseDto;
+import com.example.newsfeed.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+
+    /*
+    feat/post-read 브랜치
+    member들이 작성한 모든 글을 조회하는 메서드
+    예외처리 추가하기 -> 현재 기본기능만 구현되어있음
+    */
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> findAllPosts(){
+
+        List<PostResponseDto> postResponseDtoList = postService.findAll();
+
+        return new ResponseEntity<>(postResponseDtoList, HttpStatus.OK); // status 200
+    }
+
 }
--- src/main/java/com/example/newsfeed/post/service/PostService.java
+++ src/main/java/com/example/newsfeed/post/service/PostService.java
@@ -1,11 +1,37 @@
 package com.example.newsfeed.post.service;
 
+import com.example.newsfeed.post.dto.PostResponseDto;
+import com.example.newsfeed.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import com.example.newsfeed.member.repository.MemberRepository;
+import com.example.newsfeed.post.repository.PostRepository;
+
+
+
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    /*
+    feat/post-read 브랜치
+    모든 작성글을 찾는 서비스 JpaRepository에 스트림
+    */
+    public List<PostResponseDto> findAll(){
+
+        return postRepository.findAll().stream().map(PostResponseDto::toDto).toList();
+    }
+
+
+
+
+
 }

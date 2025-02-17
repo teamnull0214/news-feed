93033211283835349df7afd9f5b6d5e40905d893
queenriwon <queenriwon3@gmail.com>
2025-02-17 16:46:46 +0900

    Feat(login): loginMember 탈퇴한 유저 외 조회
    로그인을 할 때,
    isDelete가 false인 유저를 JPQL을 이용하여 조회 후 로그인 유저를 찾습니다.
    findActiveMemberByEmailOrElseThrow 메서드를 이용하여 isDelete가 false인 유저를 조회할 수 있습니다.

--- src/main/java/com/example/newsfeed/member/repository/MemberRepository.java
+++ src/main/java/com/example/newsfeed/member/repository/MemberRepository.java
@@ -2,11 +2,26 @@ package com.example.newsfeed.member.repository;
 
 import com.example.newsfeed.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository  extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByEmail(String email);
 
     Optional<Member> findMemberById(Long id);
+
+    // 탈퇴하지 않은 등록된 유저 중 email 값으로 조회 (로그인에 사용)
+    @Query("SELECT u FROM Member u WHERE u.id = :email AND u.isDeleted = false")
+    Optional<Member> findActiveMemberByEmail(@Param("email") String email);
+
+    // 탈퇴하지 않은 등록된 유저 중 id 값으로 조회 (타인 member-read 에 사용)
+    @Query("SELECT u FROM Member u WHERE u.id = :id AND u.isDeleted = false")
+    Optional<Member> findActiveMemberById(@Param("id") Long id);
+
+    // 탈퇴하지 않은 유저 조회 (다건 member-read 에 사용)
+    @Query("SELECT u FROM Member u WHERE u.isDeleted = false")
+    List<Member> findActiveMemberAll();
 }

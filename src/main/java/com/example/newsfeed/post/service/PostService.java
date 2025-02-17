2214e342ace56787bd4c3abcdfd521f30d393730
queenriwon <queenriwon3@gmail.com>
2025-02-17 13:46:15 +0900

    Fix(member): dev와 충돌문제 해결
    dev와 충돌이 나는 문제를 수동으로 해결하였습니다.

--- src/main/java/com/example/newsfeed/post/service/PostService.java
+++ src/main/java/com/example/newsfeed/post/service/PostService.java
@@ -4,16 +4,14 @@ import com.example.newsfeed.global.entity.BaseDateTime;
 import com.example.newsfeed.global.entity.SessionMemberDto;
 import com.example.newsfeed.member.entity.Member;
 import com.example.newsfeed.member.repository.MemberRepository;
-import com.example.newsfeed.post.dto.PostRequestDto;
-import com.example.newsfeed.post.dto.PostResponseDto;
+import com.example.newsfeed.post.dto.PostCreateRequestDto;
+import com.example.newsfeed.post.dto.PostCreateResponseDto;
 import com.example.newsfeed.post.entity.Post;
 import com.example.newsfeed.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -24,16 +22,16 @@ public class PostService extends BaseDateTime {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public PostResponseDto createPost(SessionMemberDto session, PostRequestDto requestDto) {
+    public PostCreateResponseDto createPost(SessionMemberDto session, PostCreateRequestDto requestDto) {
         Member member = Member.fromMemberId(session.getId());
         Post post = new Post(requestDto.getImage(), requestDto.getContents(), member);
         Post savedPost = postRepository.save(post);
 
         log.info("게시물 생성 성공");
-        return new PostResponseDto(
+        return new PostCreateResponseDto(
                 savedPost.getId(),
-                member.getId(),
-                savedPost.getContent(),
+                member.getNickname(),
+                savedPost.getContents(),
                 savedPost.getImage(),
                 savedPost.getCreatedAt(),
                 savedPost.getModifiedAt()

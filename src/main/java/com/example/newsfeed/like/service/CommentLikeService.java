fde1cbfdb0f6275596b86fbc8c6fb8addd894fd7
abc <clvlajrrhtlvek@gmail.com>
2025-02-19 00:45:15 +0900

    Fix(commentLike): "좋아요 생성 부분 수정"
    좋아요를 실행할 경우 모두 좋아요 기록이 존재하지
    않음을 응답하는 문제를 해결하였다.

--- src/main/java/com/example/newsfeed/like/service/CommentLikeService.java
+++ src/main/java/com/example/newsfeed/like/service/CommentLikeService.java
@@ -4,9 +4,12 @@ import com.example.newsfeed.comment.entity.Comment;
 import com.example.newsfeed.comment.service.CommentService;
 import com.example.newsfeed.like.entity.CommentLike;
 import com.example.newsfeed.like.repository.CommentLikeRepository;
+import com.example.newsfeed.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static com.example.newsfeed.like.entity.LikeStatus.LIKE;
 import static com.example.newsfeed.like.entity.LikeStatus.NOT_LIKE;
@@ -21,18 +24,25 @@ public class CommentLikeService implements LikeService {
     @Transactional
     public void createLike(Long memberId, Long commentId) {
         Comment findComment = validateNotCommentAuthor(memberId, commentId);
-        CommentLike findCommentLike = findByMemberAndCommentOrElseThrow(memberId, findComment.getId());
+        Optional<CommentLike> findCommentLike = commentLikeRepository.findByMemberIdAndCommentId(memberId, findComment.getId());
 
-        /* 이미 좋아요가 눌려있는 경우 */
-        if (findCommentLike.getLikeStatus() == LIKE) {
-            throw new RuntimeException("이미 좋아요를 누른 상태입니다.");
+        /* 좋아요 기록이 있는 경우 */
+        if (findCommentLike.isPresent()) {
+            CommentLike commentLike = findCommentLike.get();
+
+            /* 이미 좋아요가 눌려있는 경우 */
+            if (commentLike.getLikeStatus() == LIKE) {
+                throw new RuntimeException("이미 좋아요를 누른 상태입니다.");
+            }
+            commentLike.updateCommentLike(LIKE);  // 기존 좋아요 상태를 업데이트
         }
 
         /* 좋아요가 눌려있지 않은 경우 */
-        findCommentLike.updateCommentLike(LIKE);  // 기존 좋아요 상태를 업데이트
+        CommentLike commentLike = new CommentLike(new Member(memberId), findComment);
+        commentLike.updateCommentLike(LIKE);  // 기존 좋아요 상태를 업데이트
 
         // 이미 존재하는 좋아요를 업데이트한 경우, 새로 생성할 필요 없음
-        commentLikeRepository.save(findCommentLike);
+        commentLikeRepository.save(commentLike);
     }
 
     @Override

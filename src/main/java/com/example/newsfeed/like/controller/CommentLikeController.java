7c5562597ba29a5011912b0917e4ac10b626406b
abc <clvlajrrhtlvek@gmail.com>
2025-02-19 18:16:55 +0900

    Refactor(commentlike): 댓글 좋아요 성공 응답 추가
    댓글 좋아요 성공 응답을 구현했습니다.

--- src/main/java/com/example/newsfeed/like/controller/CommentLikeController.java
+++ src/main/java/com/example/newsfeed/like/controller/CommentLikeController.java
@@ -2,14 +2,19 @@ package com.example.newsfeed.like.controller;
 
 import com.example.newsfeed.global.annotation.LoginRequired;
 import com.example.newsfeed.global.dto.SessionMemberDto;
+import com.example.newsfeed.global.exception.ApiResponseDto;
+import com.example.newsfeed.global.exception.ApiResponseDtoImpl;
 import com.example.newsfeed.like.service.CommentLikeService;
+import com.example.newsfeed.post.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.example.newsfeed.global.constant.EntityConstants.LOGIN_MEMBER;
 
+@Slf4j
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
@@ -19,21 +24,31 @@ public class CommentLikeController{
 
     @LoginRequired
     @PostMapping("/{commentId}/likes")
-    public ResponseEntity<Void> createLike(
+    public ResponseEntity<ApiResponseDto<Void>> createLike(
             @PathVariable Long commentId,
             @SessionAttribute(name = LOGIN_MEMBER) SessionMemberDto session
     ) {
         commentLikeService.createLike(session.getId(), commentId);
-        return new ResponseEntity<>(HttpStatus.OK);
+
+        ApiResponseDtoImpl<Void> response = new ApiResponseDtoImpl<>();
+        response.ok(null);
+
+        log.info("좋아요 누르기 성공");
+        return ResponseEntity.ok(response);
     }
 
     @LoginRequired
     @DeleteMapping("/{commentId}/likes")
-    public ResponseEntity<Void> deleteLike(
+    public ResponseEntity<ApiResponseDto<Void>> deleteLike(
             @PathVariable Long commentId,
             @SessionAttribute(name = LOGIN_MEMBER) SessionMemberDto session
     ) {
         commentLikeService.deleteLike(session.getId(), commentId);
-        return new ResponseEntity<>(HttpStatus.OK);
+
+        ApiResponseDtoImpl<Void> response = new ApiResponseDtoImpl<>();
+        response.ok(null);
+
+        log.info("좋아요 취소 성공");
+        return ResponseEntity.ok(response);
     }
 }

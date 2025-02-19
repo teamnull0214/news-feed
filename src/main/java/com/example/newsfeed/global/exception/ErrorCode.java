2262e54c2345cc69d1f2e580b1733a9236278555
abc <clvlajrrhtlvek@gmail.com>
2025-02-19 18:42:58 +0900

    Refactor(follow): follow 상수 추가
    팔로우에 대한 예외 상수들을 추가하였습니다.

--- src/main/java/com/example/newsfeed/global/exception/ErrorCode.java
+++ src/main/java/com/example/newsfeed/global/exception/ErrorCode.java
@@ -17,7 +17,9 @@ public enum ErrorCode {
     CANNOT_FOLLOW_SELF(HttpStatus.BAD_REQUEST, "본인을 팔로우할 수 없습니다."), // 추가
     ALREADY_FOLLOWING(HttpStatus.CONFLICT, "이미 팔로우한 사용자입니다."),
     LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인 해주세요"),
-    CANNOT_UPDATE_OTHERS_DATA(HttpStatus.FORBIDDEN, "다른 사람의 정보는 수정 및 삭제가 불가능합니다.");
+    CANNOT_UPDATE_OTHERS_DATA(HttpStatus.FORBIDDEN, "다른 사람의 정보는 수정 및 삭제가 불가능합니다."),
+    ALREADY_UNFOLLOWED(HttpStatus.BAD_REQUEST, "이미 언팔로우한 유저입니다."),
+    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "팔로우 내용을 찾을 수 없습니다.");
 
 
     private final HttpStatus statusCode;

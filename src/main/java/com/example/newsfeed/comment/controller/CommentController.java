5dad68ce98821fe631fc328078ec685ff98b2e81
abc <clvlajrrhtlvek@gmail.com>
2025-02-19 12:30:41 +0900

    Refactor(post): 댓글 성공 응답 수정
    댓글의 CRUD 성공 응답 형태 통일 및 테스트 완료하였습니다. 또한 log를 통해 해당 API가 정상적으로 처리되었는지 확인할 수 있도록 하였습니다.

--- src/main/java/com/example/newsfeed/comment/controller/CommentController.java
+++ src/main/java/com/example/newsfeed/comment/controller/CommentController.java
@@ -11,6 +11,7 @@ import com.example.newsfeed.global.entity.SessionMemberDto;
 import com.example.newsfeed.global.exception.ApiResponseDto;
 import com.example.newsfeed.global.exception.ApiResponseDtoImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +19,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
-    // 추후에 Validation 사용시 각기능에 @Valid 추가
 
-    // 댓글 생성
     @LoginRequired
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<ApiResponseDto<CommentCreateResponseDto>> createComment(
@@ -37,23 +37,43 @@ public class CommentController {
         ApiResponseDtoImpl<CommentCreateResponseDto> reponse = new ApiResponseDtoImpl<>();
         reponse.ok(commentService.createComment(session, postId, requestDto));
 
+        log.info("댓글 생성 성공");
         return ResponseEntity.ok(reponse);
     }
 
+    // 추후에 Validation 사용시 각기능에 @Valid 추가
+    // 댓글 생성
     // 댓글 조회
+    // 댓글수정
+    // 해당 댓글 삭제
+    // 댓글 페이징 구현
+    @GetMapping("/posts/{postId}/page")
+    public ResponseEntity<ApiResponseDto<Page<CommentResponseDto>>> findCommentsOnPost(
+            @PathVariable Long postId,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ){
+        ApiResponseDtoImpl<Page<CommentResponseDto>> response = new ApiResponseDtoImpl<>();
+        response.ok(commentService.findCommentsOnPost(postId, page, size));
+
+        log.info("댓글 페이징 전체 조회 성공");
+        return ResponseEntity.ok(response);
+
+    }
+
     @GetMapping("/posts/{postId}/comments")
     public ResponseEntity<ApiResponseDto<List<CommentResponseDto>>> findByComment(@PathVariable Long postId){
 
         ApiResponseDtoImpl<List<CommentResponseDto>> response = new ApiResponseDtoImpl<>();
         response.ok(commentService.findByComment(postId));
 
+        log.info("댓글 전체 조회 성공");
         return ResponseEntity.ok(response);
     }
 
-    // 댓글수정
     @LoginRequired
     @PatchMapping("/posts/{postId}/comments/{commentId}")
-    public ResponseEntity<ApiResponseDtoImpl<CommentUpdateResponseDto>> updateComment(
+    public ResponseEntity<ApiResponseDto<CommentUpdateResponseDto>> updateComment(
             @SessionAttribute(name = "member") SessionMemberDto session,
             @PathVariable Long postId,
             @PathVariable Long commentId,
@@ -61,30 +81,24 @@ public class CommentController {
     ){
         ApiResponseDtoImpl<CommentUpdateResponseDto> response = new ApiResponseDtoImpl<>();
         response.ok(commentService.updateComment(session, postId, commentId, requestDto));
+
+        log.info("댓글 수정 성공");
         return ResponseEntity.ok(response);
     }
 
-    // 해당 댓글 삭제
     @LoginRequired
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
-    public ResponseEntity<Void> deleteComment(
+    public ResponseEntity<ApiResponseDto<Void>> deleteComment(
             @SessionAttribute(name = "member") SessionMemberDto session,
             @PathVariable Long postId,
             @PathVariable Long commentId
     ){
         commentService.deleteComment(session, postId, commentId);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 
-    // 댓글 페이징 구현
-    @GetMapping("/posts/{postId}/page")
-    public ResponseEntity<Page<CommentResponseDto>> findCommentsOnPost(
-            @PathVariable Long postId,
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ){
-        Page<CommentResponseDto> result = commentService.findCommentsOnPost(postId, page, size);
-        return ResponseEntity.ok(result);
+        ApiResponseDtoImpl<Void> response = new ApiResponseDtoImpl<>();
+        response.ok(null);
 
+        log.info("댓글 삭제 성공");
+        return ResponseEntity.ok(response);
     }
 }
